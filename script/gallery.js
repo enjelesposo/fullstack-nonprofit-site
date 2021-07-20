00ae63fef7d8e8d1bf437fff7fbb2c60ba5a9fd1
Angelica Esposo <73009213+enjelesposo@users.noreply.github.com>
2021-07-20 14:36:44 +0800

    Delete script directory

--- script/gallery.js
+++
@@ -1,7 +0,0 @@
-const thumbnails = document.querySelectorAll('.thumbnail');
-
-thumbnails.forEach( thumbnail => {
-    thumbnail.addEventListener('click', e => {
-        console.log(e.target);
-    })
-})
